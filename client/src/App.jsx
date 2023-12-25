865561d2110463003379b12c9f4a9a8a212b2c30
Panupong <Pai28212m@gmail.com>
2023-12-25 23:34:21 +0700

    HH:Pai(Revised)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -58,24 +58,6 @@ function App() {
             </div>
           );
         })}
-
-        <div className="product">
-          <div className="product-preview">
-            <img
-              src="https://via.placeholder.com/350/350"
-              alt="some product"
-              width="350"
-              height="350"
-            />
-          </div>
-          <div className="product-detail">
-            <h1>Product name: ...</h1>
-            <h2>Product price: ... Baht</h2>
-            <p>Product description: .....</p>
-          </div>
-
-          <button className="delete-button">x</button>
-        </div>
       </div>
     </div>
   );
